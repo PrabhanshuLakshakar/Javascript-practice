2af46273f085dbb601a50b71b3410a95f230440a
Prabhanshu_Lakshakar <128672330+PrabhanshuLakshakar@users.noreply.github.com>
2023-09-05 15:02:08 +0530

    Add files via upload

---
+++ 03-conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof score);
+//console.log(valueInNumber);
+
+
+//notes  
+// " 33" => 33
+//"33abc" => NaN
+//true => 1; false=>0
+
+let isLoggedIn = "prabhanshu"
+ //let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//notes
+// 1 => true , 0 => false
+//" "=> false
+// "prabhanshu" => true
+
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(stringNumber);

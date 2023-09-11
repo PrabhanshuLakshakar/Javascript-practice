20e18bd77292804144d7bd85314fba8850c9fdfe
Prabhanshu_Lakshakar <128672330+PrabhanshuLakshakar@users.noreply.github.com>
2023-09-11 09:28:53 +0530

    Add files via upload

---
+++ 09-datesin.js
@@ -0,0 +1,28 @@
+//dates
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+//let myCreateDate = new Date(2023,0,23)
+let myCreatedDate = new Date(2002,4,7,5,3)
+//console.log(myCreateDate.toLocaleString());
+
+//let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleDateString('default',{
+    weekday:"long",
+
+})
